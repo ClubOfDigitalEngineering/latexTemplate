6863b9c4fa6ac6c9527ed3944c09d1062b421ad7
Soeren Peters - iRMB <peters.soeren@gmx.net>
2017-03-29 15:26:55 +0200

    - change bibtex to biblatex - bugfixes in verzeichnisse - new titlepagestyle

--- main.for
+++
@@ -1,6 +0,0 @@
-\select@language {ngerman}
-\addvspace {10\p@ }
-\contentsline {formel}{\numberline {\relax 1.1} Probleml\IeC {\"o}sezeit seriell bei schwacher Skalierung}{3}{equation.1.4.1}
-\addvspace {10\p@ }
-\addvspace {10\p@ }
-\addvspace {10\p@ }
